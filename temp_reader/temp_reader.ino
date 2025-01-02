ca586b2c4f53ae863e90b2f301e8d6669fc1af9d
Simon Gautier <gautier.simon@gmail.com>
2025-01-02 20:15:02 +0100

    Ajout script de test de l'écran

--- temp_reader/temp_reader.ino
+++ temp_reader/temp_reader.ino
@@ -4,30 +4,30 @@
 #define RESISTOR 100000 // résistance de la résistance, 100 kOhm
 #define THERMISTOR 100000 // résistance nominale de la thermistance, 100 kOhm
 #define NOMINAL 25 // température nominale
- 
+
 #define sensor A1
 
-#define NB_SAMPLES 100
+#define NB_SAMPLES 10
 
 void setup() {
     Serial.begin(9600);
     pinMode(sensor, INPUT);
 }
- 
+
 void loop() {
   float totalTemperature = 0;
   for (int i=1 ; i <= NB_SAMPLES ; i++) {
     totalTemperature += getTemperature();
     delay(200);
   }
-  
+
   Serial.println(totalTemperature / NB_SAMPLES);
 }
 
 float getTemperature() {
   int t = analogRead(sensor);
   float tr = 1023.0 / t - 1;
-  tr = RESISTOR / tr; 
+  tr = RESISTOR / tr;
   float steinhart;
   steinhart = tr / THERMISTOR;
   steinhart = log(steinhart);
